ffe1e51b21ca9d9298086b05f3899b087879b8e2
Fhmi00 <anisfahmi045@gmail.com>
2022-10-01 11:05:58 +0700

    fix padding

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import logo from "../../assets/img/logo.png";
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="px-5">
       <Container fluid>
         <Navbar.Brand href="#">
           <img src={logo} alt="" />
@@ -22,7 +22,7 @@ function Header() {
             <Nav.Link href="#action2">Create Event</Nav.Link>
             <Nav.Link href="#action3">Location</Nav.Link>
           </Nav>
-          <div className="d-flex me-5 gap-3">
+          <div className="d-flex gap-3">
             <Button
               variant="outline-light"
               className="rounded-4 px-5 text-dark"
